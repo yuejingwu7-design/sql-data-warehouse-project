763f5756d9e0fab6abb8909a15715c11e6b53e0c
yuejingwu7-design <yuejingwu7@gmail.com>
2025-09-05 16:05:50 +0800

    Update proc_load_EDW.sql

--- scripts/EDW/proc_load_EDW.sql
+++ scripts/EDW/proc_load_EDW.sql
@@ -1,4 +1,21 @@
+/*
+=====================================================================================
+Stored Procedure: Load Silver Layer (stage -> EDW)
+=====================================================================================
+Script Purpose:
+	This stored procedure performs the ETL (Extract, Transform, Load) process to populate the 'silver' schema tables from the 'bronze' schema.
+  Actions Performed:
+	- Truncates Silver tables.
+	- Inserts transformed and cleansed data from Bronze into Silver tables.
 
+Parameters:
+	None.
+	This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+	EXEC Silver.load_silver;
+======================================================================================
+*/
 
 CREATE or ALTER PROCEDURE EDW.load_EDW AS
 BEGIN
